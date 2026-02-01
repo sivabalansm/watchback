6aeb07f604c23839117353bbcfb96538e60a3385
Sivabalan Muthurajan (Velup) <sivabalansm@yahoo.com>
2026-02-01 02:23:40 -0500

    WIP Add example partially

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+// Working on example over here: https://docs.rs/notify/latest/notify/
+use notify::{Event, RecursiveMode, Result, Watcher};
+use std::{path::Path, sync::mspc};
+
+fn main() -> Result<()> {
+    let (tx, rx) = mspc::channel::<Result<Event>>();
+
+    let mut watcher = notify::recommended_watcher(tx)?;
 }
