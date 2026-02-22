dac8b9f171ed6805f2d343dff1bede6e3ca8d414
Sivabalan Muthurajan (Velup) <sivabalansm@yahoo.com>
2026-02-21 22:43:34 -0500

    Add filtering event kinds

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 // Working on example over here: https://docs.rs/notify/latest/notify/
 use notify::{Event, RecursiveMode, Result, Watcher};
-use std::{path::Path, sync::mspc};
+use notify::event;
+use std::{path::Path, sync::mpsc};
 
 fn main() -> Result<()> {
-    let (tx, rx) = mspc::channel::<Result<Event>>();
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
 
     let mut watcher = notify::recommended_watcher(tx)?;
+
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                println!("event kind: {:?}", event.kind);
+                // println!("event: {:?}", event);
+                match event.kind {
+                    event::EventKind::Create(event::CreateKind::File) => println!("new file created!"),
+                    _ => println!("Not implemented")
+                }
+            },
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
 }
